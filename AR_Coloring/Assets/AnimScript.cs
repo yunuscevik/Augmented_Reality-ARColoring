895877a6bf117570bf9f68fd98d1b19596779b6a
Yunus ÇEVİK <yunus71_06@hotmail.com>
2019-01-21 10:40:42 +0300

    Initial commit

---
+++ AR_Coloring/Assets/AnimScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimScript : MonoBehaviour {
+
+    Animator AnimatorComponent;
+    // Use this for initialization
+    void Start()
+    {
+        AnimatorComponent = (GameObject.Find("uganda")).GetComponent<Animator>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+               
+    }
+
+    public void trun360()
+    {
+        AnimatorComponent.Play("360", -1, 0f);
+    }
+
+    public void jump()
+    {
+        AnimatorComponent.Play("jump", -1, 0f);
+    }
+
+    public void jumpAnd360()
+    {
+        AnimatorComponent.Play("jumpAnd360", -1, 0f);
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
